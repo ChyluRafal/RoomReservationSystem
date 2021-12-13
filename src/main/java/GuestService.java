ea97fd26b797f17c0910650c24ab7fb339091829
ChyluRafal <rafalchylinski93@gmail.com>
2021-12-13 16:30:43 +0100

    commit complite base

---
+++ src/main/java/GuestService.java
@@ -0,0 +1,12 @@
+public class GuestService {
+    GuestRepository repository = new GuestRepository();
+    public Guest createNewGuest(String firstName, String lastName, int age, int genderOption) {
+        Gender gender = Gender.FEMALE;
+        if(genderOption==1) {
+            gender = Gender.MALE;
+        } else if(genderOption==2) {
+            gender = Gender.FEMALE;
+        }
+        return repository.createNewGuest(firstName, lastName, age, gender);
+    }
+}
---
+++ src/main/java/TextUI.java
@@ -0,0 +1,52 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class TextUI {
+    GuestService guestService = new GuestService();
+    RoomService roomService = new RoomService();
+    public void readNewGuestData(Scanner input) {
+        System.out.println("Tworzymy nowego gościa.");
+        try {
+            System.out.println("Podaj imię: ");
+            String firstName = input.next();
+            System.out.println("Podaj nazwisko: ");
+            String lastName = input.next();
+            System.out.println("Podaj wiek: ");
+            int age = input.nextInt();
+            System.out.println("Podaj płeć (1. Mężczyzna, 2. Kobieta)");
+            int genderOption = input.nextInt();
+            if(genderOption != 1 && genderOption !=2) {
+                throw new WrongOptionException("Wrong option in gender selection");
+            }
+            Guest newGuest = guestService.createNewGuest(firstName, lastName, age, genderOption);
+            System.out.println(newGuest.getInfo());
+        } catch (InputMismatchException e) {
+            throw new OnlyNumberException("Use only numbers when choosing data");
+        }
+    }
+    public void readNewRoomData(Scanner input) {
+        System.out.println("Tworzymy nowy pokój.");
+        try {
+            System.out.println("Numer: ");
+            int number = input.nextInt();
+            int[] bedTypes = chooseBedType(input);
+            Room newRoom = roomService.createNewRoom(number, bedTypes);
+            System.out.println(newRoom.getInfo());
+        } catch (InputMismatchException e) {
+            throw new OnlyNumberException("Use numbers when creating new room");
+        }
+    }
+    static private int[] chooseBedType(Scanner input) {
+        System.out.println("Ile łóżek w pokoju?: ");
+        int bedNumber = input.nextInt();
+        int[] bedTypes = new int[bedNumber];
+        for(int i=0;i<bedNumber;i=i+1) {
+            System.out.println("Typy łóżek: ");
+            System.out.println("\t1. Pojedyncze");
+            System.out.println("\t2. Podwójne");
+            System.out.println("\t3. Królewskie");
+            int bedTypeOption = input.nextInt();
+            bedTypes[i] = bedTypeOption;
+        }
+        return bedTypes;
+    }
+}
